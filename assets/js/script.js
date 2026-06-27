2b22d0fef1f2c071fc2b16938f451f6e3d224f28
m1buildingllc <asaturasatryan98@gmail.com>
2026-06-28 03:13:18 +0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,37 @@ function setLanguage(lang){
     alert("Selected language: " + lang);
 
 }
+
+function setLanguage(lang){
+
+    if(lang === "en"){
+
+        document.getElementById("heroTitle").innerHTML =
+        "Building Quality.<br>Creating Value.";
+
+        document.getElementById("heroText").innerHTML =
+        "Professional construction, landscaping and infrastructure solutions delivered with quality, precision and reliability.";
+
+    }
+
+    if(lang === "hy"){
+
+        document.getElementById("heroTitle").innerHTML =
+        "Կառուցում ենք որակ,<br>ստեղծում արժեք։";
+
+        document.getElementById("heroText").innerHTML =
+        "Մենք իրականացնում ենք բարձրորակ շինարարական և բարեկարգման աշխատանքներ։";
+
+    }
+
+    if(lang === "ru"){
+
+        document.getElementById("heroTitle").innerHTML =
+        "Строим качество.<br>Создаем ценность.";
+
+        document.getElementById("heroText").innerHTML =
+        "Мы выполняем строительные и благоустроительные работы высокого качества.";
+
+    }
+
+}
