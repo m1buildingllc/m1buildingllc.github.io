99f8348ee1aa4ff15111c6daa0f9a2f2b6b7ec2b
m1buildingllc <asaturasatryan98@gmail.com>
2026-06-28 14:57:53 +0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,3 +64,24 @@ document.querySelectorAll('nav a').forEach(anchor => {
     });
 
 });
+
+const reveals = document.querySelectorAll(".reveal");
+
+function revealSections() {
+
+    reveals.forEach(section => {
+
+        const windowHeight = window.innerHeight;
+        const revealTop = section.getBoundingClientRect().top;
+        const revealPoint = 120;
+
+        if (revealTop < windowHeight - revealPoint) {
+            section.classList.add("active");
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll", revealSections);
+revealSections();
