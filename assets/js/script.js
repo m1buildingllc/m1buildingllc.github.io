46534611318a4c8dddae354082497b0be1e19a76
m1buildingllc <asaturasatryan98@gmail.com>
2026-06-28 14:52:34 +0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,23 @@ window.addEventListener("scroll", () => {
     });
 
 });
+
+document.querySelectorAll('nav a').forEach(anchor => {
+
+    anchor.addEventListener('click', function (e) {
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if (target) {
+
+            e.preventDefault();
+
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+
+        }
+
+    });
+
+});
